ab633ab36afc775378737c262231767bf6bd376a
Adekunle Oluwatobiloba <146554573+Destiny27052005@users.noreply.github.com>
2026-02-13 02:45:21 +0100

    Section component updated

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -9,12 +9,12 @@ function Section() {
     return (
         <section className="">
             <Card
-                className="md:grid grid-cols-2" classname="hidden md:block w-full"
+                className="md:grid grid-cols-2" classname="hidden md:block w-full h-full"
                 deskImg={desktopTransform} mobileImg={mobileTransform} alt="Transform"
                 title="Transform your brand"
                 text="We are a full-service creavite agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you." />
             <Card
-                className="md:grid  grid-cols-2" classname="hidden  md:block order-1 w-full"
+                className="md:grid  grid-cols-2" classname="hidden  md:block order-1 w-full  h-full"
                 deskImg={desktopStand} mobileImg={mobileStand} alt="Stand out"
                 title="Stand out to the right audience"
                 text="Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we'll build and extend your brand in digital places." />
